1474439b3e440fea2494848214a1cb6e490fce2e
nunojorge <nujo@hotmail.com>
2014-01-27 19:29:32 -0500

    Configuring root as pictures#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Photogur::Application.routes.draw do
+  root :to => "pictures#index"
+  get 'pictures' => 'pictures#index'
+
+
   get 'pictures' => 'pictures#index'
 
   post 'pictures' => "pictures#create" # this is a new line of code
